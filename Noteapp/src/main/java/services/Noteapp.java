a166fe62d5385b7174eacdf100f1d95dd3c966b2
Nyyri <75020126+Nyyri@users.noreply.github.com>
2022-10-05 20:34:47 +0300

    New services

--- Noteapp/src/main/java/services/Noteapp.java
+++ Noteapp/src/main/java/services/Noteapp.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
@@ -13,49 +14,72 @@ import javax.ws.rs.core.MediaType;
 
 import conn.Connections;
 import data.*;
+import dao.Dao;
 
 @Path("noteappservice")
 public class Noteapp {
-
+	
 	@POST
 	@Path("/checkuserlogin")
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	public boolean checkUserLogin(User u) {
-		Connection conn = null;
-
-		try {
-			conn = Connections.getDevConnection();
-		} catch (Exception e) {
-			e.printStackTrace();
-			return false;
-		}
-
-		try {
-			String sql = "SELECT UserID, Username FROM users WHERE username = ? AND password = ?;";
-			PreparedStatement pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, u.getUsername());
-			pstmt.setString(2, u.getPassword());
-			ResultSet result = pstmt.executeQuery();
-			if (result.next()) {
-				return true;
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			try {
-				if (conn != null)
-					conn.close();
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-
-		return false;
+		return Dao.checkUserLogin(u.getUsername(), u.getPassword());
+	}
+	
+	@POST
+	@Path("/registeruser")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public boolean registerUser(User u) {
+		return Dao.addNewUser(u.getUsername(), u.getPassword());
+	}
+	
+	@POST
+	@Path("/getusernotes")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public ArrayList<Note> getUserNotes(User u) {
+		return Dao.getUserNotes(u.getUserID());
+	}
+	
+	@POST
+	@Path("/getnotessharedtouser")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public ArrayList<Note> getNotesSharedToUser(User u) {
+		return Dao.getNotesSharedToUser(u.getUserID());
+	}
+	
+	@POST
+	@Path("/getpublicnotes")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public ArrayList<Note> getPublicNotes(User u) {
+		return Dao.getPublicNotes(u.getUserID());
+	}
+	
+	@POST
+	@Path("/getusers")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public ArrayList<User> getUsers(User u) {
+		return Dao.getUsers(u.getUserID());
+	}
+	
+	@POST
+	@Path("/getnote")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Note getNote(Note n) {
+		return Dao.getNote(n.getNoteID());
+	}
+	
+	@POST
+	@Path("/addnote")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public boolean addNote(Note n) {
+		return Dao.addNewNote(n.getTitle(), n.getContent(), n.getDate(), n.isNotePublic(), n.getUserID());
 	}
 }
